491c0d4c4a1d7811ef742a7833ab35a9bc644711
Sama-Mahmoud <samafawzy373@gmail.com>
2024-08-18 13:01:09 +0300

    some small changes

---
+++ Contract/AuthDtos/AuthWithGoogle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract.AuthDtos
+{
+    public class GoogleAuthDto
+    {
+        public string? Provider { get; set; }
+        public string? IdToken { get; set; }
+    }
+}
