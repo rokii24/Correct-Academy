6f22576588d4ebe7c762526fa3b2a8ba69830138
Sama-Mahmoud <samafawzy373@gmail.com>
2024-08-28 18:34:57 +0300

    chat service

---
+++ Service/DataServices/AdminDataService.cs
@@ -0,0 +1,26 @@
+﻿using Domain.IRepositories.DataRepository;
+using Service.Abstraction.IDataServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DataServices
+{
+    public class AdminDataService : IAdminDataService
+    {
+        private readonly IChatService _chatService;
+        private readonly IPostService _postServices;
+
+        public AdminDataService(IAdminDataRepository adminDataRepository)
+        {
+            _chatService=new ChatService();
+            _postServices=new PostService();
+        }
+
+        public IChatService ChatService => _chatService;
+
+        public IPostService PostService => _postServices;
+    }
+}
---
+++ Service/DataServices/PostService.cs
@@ -0,0 +1,13 @@
+﻿using Service.Abstraction.IDataServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DataServices
+{
+    public class PostService : IPostService
+    {
+    }
+}
