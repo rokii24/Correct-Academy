8e4227c52f4c7c9372faca9c2f6f560c33a5a3d3
Israa Lotfy <israalotfy891@gmail.com>
2024-09-07 08:59:22 +0300

    zz

--- Persistence/Reposetories/DAtaReposatoiry/CertificateReposatiory.cs
+++ Persistence/Reposetories/DAtaReposatoiry/CertificateReposatiory.cs
@@ -1,63 +1,63 @@
-﻿using Domain.Entities.DataEntities;
-using Domain.IRepositories.DataRepositories;
-using Google;
-using Microsoft.EntityFrameworkCore;
-using Persistence.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static Persistence.Reposetories.DAtaReposatoiry.CertificateReposatiory;
+﻿//using Domain.Entities.DataEntities;
+//using Domain.IRepositories.DataRepositories;
+//using Google;
+//using Microsoft.EntityFrameworkCore;
+//using Persistence.Context;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using static Persistence.Reposetories.DAtaReposatoiry.CertificateReposatiory;
 
-namespace Persistence.Reposetories.DAtaReposatoiry
-{
-    public class CertificateReposatiory : ICertificateRepository
-    {
-        private readonly CorrectAcademyContext _context;
+//namespace Persistence.Reposetories.DAtaReposatoiry
+//{
+//    public class CertificateReposatiory : ICertificateRepository
+//    {
+//        private readonly CorrectAcademyContext _context;
 
-        public CertificateReposatiory(CorrectAcademyContext context)
-        {
-            _context = context;
-        }
+//        public CertificateReposatiory(CorrectAcademyContext context)
+//        {
+//            _context = context;
+//        }
 
-        public async Task<Certificate> GetByIdAsync(int id)
-        {
+//        public async Task<Certificate> GetByIdAsync(int id)
+//        {
 
-            return await _context.Certificates
-                .Include(c => c.Student)
-                .FirstOrDefault(c => c.Id == id);
-        }
+//            return await _context.Certificates
+//                .Include(c => c.Student)
+//                .FirstOrDefault(c => c.Id == id);
+//        }
 
-        public async Task<IEnumerable<Certificate>> GetAllAsync()
-        {
-            return await _context.Certificates
-                .Include(c => c.Student) // Assuming you want to include the related Student entity
-                .ToListAsync();
-        }
+//        public async Task<IEnumerable<Certificate>> GetAllAsync()
+//        {
+//            return await _context.Certificates
+//                .Include(c => c.Student) // Assuming you want to include the related Student entity
+//                .ToListAsync();
+//        }
 
-        public async Task AddAsync(Certificate certificate)
-        {
-            await _context.Certificates.AddAsync(certificate);
-            await _context.SaveChangesAsync();
-        }
+//        public async Task AddAsync(Certificate certificate)
+//        {
+//            await _context.Certificates.AddAsync(certificate);
+//            await _context.SaveChangesAsync();
+//        }
 
-        public async Task UpdateAsync(Certificate certificate)
-        {
-            _context.Certificates.Update(certificate);
-            await _context.SaveChangesAsync();
-        }
+//        public async Task UpdateAsync(Certificate certificate)
+//        {
+//            _context.Certificates.Update(certificate);
+//            await _context.SaveChangesAsync();
+//        }
 
-        public async Task DeleteAsync(string id)
-        {
-            var certificate = await _context.Certificates.FindAsync(id);
-            if (certificate != null)
-            {
-                _context.Certificates.Remove(certificate);
-                await _context.SaveChangesAsync();
-            }
-        }
-    }
+//        public async Task DeleteAsync(string id)
+//        {
+//            var certificate = await _context.Certificates.FindAsync(id);
+//            if (certificate != null)
+//            {
+//                _context.Certificates.Remove(certificate);
+//                await _context.SaveChangesAsync();
+//            }
+//        }
+//    }
 
-}
+//}
 
