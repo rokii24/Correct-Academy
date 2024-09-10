915589940ed1c9f3ed8dc514ea731d674cc1c490
shroukmohamed 30203242200708 <shroukmohamed896_sd@fcis.bsu.edu.eg>
2024-09-10 16:44:12 +0300

    cerfifcation repo dn

---
+++ Domain/IRepositories/DataRepositories/ICategoryReposatiory.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.DataEntities;
+using Domain.IReposetories.DataRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.IRepositories.DataRepositories
+{
+    public interface ICategoryReposatiory : IBaseRepository<Category>
+    {
+    }
+}
