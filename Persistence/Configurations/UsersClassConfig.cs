db9d2c5064be90a081a9c5d7c6e2a678730c6330
Sama-Mahmoud <samafawzy373@gmail.com>
2024-09-17 14:48:26 +0300

    reposs

--- Persistence/Configurations/UsersClassConfig.cs
+++ Persistence/Configurations/UsersClassConfig.cs
@@ -2,11 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Persistence.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Persistence.Configurations
 {
